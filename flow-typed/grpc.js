c1ee9c8db865f47832481f912ee872d407aef1e0
Romeo Van Snick <romeovs@gmail.com>
2017-07-12 16:03:39 +0200

    Remove flw-typed folder from repo and move proto types into src

--- flow-typed/grpc.js
+++
@@ -1,7 +0,0 @@
-// Copyright © 2017 The Things Network
-// Use of this source code is governed by the MIT license that can be found in the LICENCE file.
-
-
-declare module "grpc" {
-  declare module.exports : any
-}
